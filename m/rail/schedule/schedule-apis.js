3bf33ee7d8f0ea5602b962730d0db40f9ad6f57f
Chris Wraith <chris@indigenously.co.uk>
2018-12-06 22:27:51 +0000

    Initial check-in

---
+++ m/rail/schedule/schedule-apis.js
@@ -0,0 +1,34 @@
+class ScheduleApis {
+    createUrl(path) {
+        return 'https://www.indigenously.co.uk/refdata/api/'
+            .concat(path);
+    }
+
+    doSearchLocations(location, cb) {
+        fetch(this.createUrl('location/search?q=' + location))
+            .then(function (response) {
+                return response.json();
+            })
+            .then(cb);
+    }
+
+    doJourneyElementsSearch(departureDate, searchType, serviceDescription, departureStation, cb) {
+        fetch(this.createUrl('schedule/journey-elements?date=' + departureDate +
+            '&' + searchType + '=' + serviceDescription +
+            '&departureStation=' + departureStation))
+            .then(function (response) {
+                return response.json();
+            })
+            .then(cb);
+    }
+
+    doScheduleSearch(departureDate, searchType, serviceDescription, departureStation, cb) {
+        fetch(this.createUrl('schedule?date=' + departureDate +
+            '&' + searchType + '=' + serviceDescription +
+            '&departureStation=' + departureStation))
+            .then(function (response) {
+                return response.json();
+            })
+            .then(cb);
+    }
+}
