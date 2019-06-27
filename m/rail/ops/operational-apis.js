bace4b0f59b7207d3d43f63da9817c359bf38269
Chris Wraith <chris@indigenously.co.uk>
2019-06-27 22:21:27 +0100

    Adding rail ops

---
+++ m/rail/ops/operational-apis.js
@@ -0,0 +1,27 @@
+class OperationalApis {
+    createUrl(path) {
+        //return 'https://www.indigenously.co.uk/operational/api/v1/'
+        return 'https://embarcadero.pv.indigenously.net:14081/operational/api/v1/'
+            .concat(path);
+    }
+
+    getAreas(cb) {
+        fetch(this.createUrl('ops/areas'), {
+            credentials: 'include'
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(cb);
+    }
+
+    getAreaStatus(areaId, cb) {
+        fetch(this.createUrl('ops/' + areaId), {
+            credentials: 'include'
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(cb);
+    }
+}
