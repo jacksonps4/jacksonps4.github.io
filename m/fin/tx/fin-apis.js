396658083d6491b38c8a574894979e70d496d03f
Chris Wraith <chris@indigenously.co.uk>
2019-08-09 23:33:22 +0100

    Adding transaction report

---
+++ m/fin/tx/fin-apis.js
@@ -0,0 +1,16 @@
+class FinApis {
+    createUrl(path) {
+        return 'https://www.indigenously.co.uk/finman/api'
+            .concat(path);
+    }
+
+    getRecentTransactions(cb) {
+        fetch(this.createUrl('transactions/recent'), {
+            credentials: 'include'
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(cb);
+    }
+}
