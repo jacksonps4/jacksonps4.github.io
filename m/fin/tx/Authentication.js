8f835833e3b459b984d298206292739a3a6e221e
Chris Wraith <chris@indigenously.co.uk>
2019-08-23 15:49:48 +0100

    Adding Google authentication

---
+++ m/fin/tx/Authentication.js
@@ -0,0 +1,11 @@
+class Authentication {
+    onSignIn(googleUser) {
+        let profile = googleUser.getBasicProfile();
+
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+        console.log('Token: ' + googleUser.getAuthResponse().id_token)
+    }
+}
